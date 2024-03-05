091e8e5b74d9811b876ad6f09f6241b5d9406c97
Biswa-Zielotech <128882616+amaparivar@users.noreply.github.com>
2024-03-05 13:23:57 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -10,8 +10,8 @@ public class EvenOdd {
         int num = reader.nextInt();
 
         if(num % 2 == 0)
-            System.out.println(num + " is Even");
+            System.out.println(num + " is even");
         else
-            System.out.println(num + " is Odd");
+            System.out.println(num + " is odd");
     }
 }
